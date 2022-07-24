85f72ce345d8a9247b79a4b6aa76c97758e6f517
VirtualFabis <faviangamerx@gmail.com>
2022-07-24 00:31:14 -0500

    Fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { CreateEmpleadoComponent } from './components/create-empleado/create-emp
 import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 
 const routes: Routes = [
- 
-  
-  { path: '**', redirectTo: 'list-empleados', pathMatch: 'full' },
-];
+  { path: '', redirectTo: 'list-empleados', pathMatch: 'full' },
+  { path: 'list-empleados', component: ListEmpleadosComponent },
+  { path: 'create-empleado', component: CreateEmpleadoComponent },
+  { path: 'editEmpleado/:id', component: CreateEmpleadoComponent },
+  { path: '**', redirectTo: 'list-empleados', pathMatch: 'full' },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
